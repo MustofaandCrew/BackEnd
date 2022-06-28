6f0cc1b4e28b103a9a4cd0c56a3e492b807beff7
SatrioBayu <sbayu875@gmail.com>
2022-06-28 13:09:39 +0700

    feat: edit category testing

--- app/controller/category.spec.js
+++ app/controller/category.spec.js
@@ -1,9 +1,10 @@
-const categoryController = require('./categoryController');
+const categoryController = require("./categoryController");
 // const { sequelize } = require('../models');
-const { array } = require('../../middleware/multer');
+const { array } = require("../../middleware/multer");
 // const { queryInterface } = sequelize;
 
-const { Category } = require('../models');
+const { Category } = require("../models");
+const { Op } = require("sequelize");
 // beforeAll(async () => {
 //     await queryInterface.bulkInsert('Category', [
 //         {
@@ -20,314 +21,273 @@ const { Category } = require('../models');
 //         restartIdentity: true,
 //     });
 // });
+beforeAll(async () => {
+  await Category.create({
+    id: 3,
+    nama: "Pakaian Pria",
+  });
+});
 
-// afterAll( async () => {
-//     await Category.destroy({
-//         where: { 
-//             nama: "Elektronik",
-//         },
-//     });
-// });
+afterAll(async () => {
+  await Category.destroy({
+    where: {
+      nama: "Pakaian Wanita",
+    },
+  });
+});
 
-describe('Category Controller', () => {
-    describe('#getListCategories() WithContent', () => {
-        beforeEach(async () => {
-            await Category.create({
-                nama: "Elektronik"
-            })
-        });
-        afterAll(async() => {
-            await Category.destroy({
-                where: {
-                    nama: "Elektronik",
-                }
-            })
-        });
+describe("Category Controller", () => {
+  describe("#getListCategories() WithContent", () => {
+    it("should return all categories success 200", async () => {
+      const mockRequest = {};
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      };
 
-        it('should return all categories success 200', async () => {
-            const mockRequest = {};
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn().mockReturnThis(),
-            };
-            
-            await categoryController.getListCategories(mockRequest, mockResponse);
-            expect(mockResponse.status).toHaveBeenCalledWith(200);
-            expect(mockResponse.json).toHaveBeenCalledWith({
-                message: expect.any(String),
-                data: expect.any(Array),
-            });
-        });
+      await categoryController.getListCategories(mockRequest, mockResponse);
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: expect.any(String),
+        data: expect.any(Array),
+      });
     });
+  });
 
-    describe('#getListCategories() NoContent', () => {
-        it('No Content status 204', async () => {
-            const mockRequest = {};
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                end: jest.fn().mockReturnThis(),
-            };
-            
-            await categoryController.getListCategories(mockRequest, mockResponse);
-            expect(mockResponse.status).toHaveBeenCalledWith(204);
-            expect(mockResponse.end).toHaveBeenCalled();
-        });
+  describe("#getListCategoriesById()", () => {
+    it("should return a list of categories success 200", async () => {
+      const mockRequest = {
+        params: {
+          id: 1,
+        },
+      };
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      };
+
+      await categoryController.getListCategoriesById(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: expect.any(String),
+        data: expect.any(Object),
+      });
     });
+    it("should return 404 not found", async () => {
+      const mockRequest = {
+        params: {
+          id: 12,
+        },
+      };
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      };
 
-    describe('#getListCategoriesById() WithContent', () => {
-        beforeEach(async () => {
-            await Category.create({
-                id: 1,
-                nama: "Elektronik"
-            })
-        });
-        afterAll(async() => {
-            await Category.destroy({
-                where: {
-                    nama: "Elektronik",
-                }
-            })
-        });
-        it('should return a list of categories success 200', async () => {
-            const mockRequest = {
-                params: {
-                    id: 1,
-                },
-            };
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn().mockReturnThis()
-            };
-
-            await categoryController.getListCategoriesById(mockRequest, mockResponse);
-
-            expect(mockResponse.status).toHaveBeenCalledWith(200);
-            expect(mockResponse.json).toHaveBeenCalledWith({
-                message: expect.any(String),
-                data: expect.any(Object)
-            });
-        });
+      await categoryController.getListCategoriesById(mockRequest, mockResponse);
+      expect(mockResponse.status).toHaveBeenCalledWith(404);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: expect.any(String),
+            message: expect.any(String),
+          },
+        ],
+      });
     });
+    it("should return a an error 400", async () => {
+      const mockRequest = {};
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      };
 
-    describe('#getListCategoriesById() NotFound', () => {
-        it('should return a list of categories NotFound 404', async () => {
-            const mockRequest = {
-                params: {
-                    id: 12
-                },
-            };
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn().mockReturnThis()
-            };
-
-            await categoryController.getListCategoriesById(mockRequest, mockResponse);
-            expect(mockResponse.status).toHaveBeenCalledWith(404);
-            expect(mockResponse.json).toHaveBeenCalledWith({
-                errors: [
-                    {
-                      code: expect.any(String),
-                      message: expect.any(String),
-                    },
-                  ],
-            });
-        });
+      await categoryController.getListCategoriesById(mockRequest, mockResponse);
+      expect(mockResponse.status).toHaveBeenCalledWith(400);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        error: expect.any(String),
+      });
     });
+  });
 
-    describe('#createCategory()', () => {
-        it('should create a new category success 201', async () => {
-            const mockRequest = {
-                body: {
-                    nama: "Elektronik",
-                },
-            };
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn().mockReturnThis(),
-            };
+  describe("#createCategory()", () => {
+    it("should create a new category success 201", async () => {
+      const mockRequest = {
+        body: {
+          nama: "Pakaian Wanita",
+        },
+      };
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      };
 
-            await categoryController.createCategory(mockRequest, mockResponse);
+      await categoryController.createCategory(mockRequest, mockResponse);
 
-            expect(mockResponse.status).toHaveBeenCalledWith(201);
-            expect(mockResponse.json).toHaveBeenCalledWith({
-                message: expect.any(String),
-                data: expect.any(Object),
-            });
-        });
+      expect(mockResponse.status).toHaveBeenCalledWith(201);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: expect.any(String),
+        data: expect.any(Object),
+      });
     });
+    it("should return 400 if there's an error", async () => {
+      const mockRequest = {};
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      };
 
-    describe('#updateCategory() Success', () => {
-        beforeEach(async () => {
-            await Category.create({
-                id: 1,
-                nama: "Elektronik"
-            })
-        });
-        afterAll(async() => {
-            await Category.destroy({
-                where: {
-                    nama: "Elektronik",
-                }
-            })
-        });
-        it('should update the category success 200', async () => {
-            const mockRequest = {
-                params: {
-                    id:1
-                },
-                body: {
-                    nama: "kendaraan",
-                }
-            };
-            const mockResponse = {
-                json: jest.fn().mockReturnThis(),
-                status: jest.fn().mockReturnThis(),
-            };
+      await categoryController.createCategory(mockRequest, mockResponse);
 
-            await categoryController.updateCategory(mockRequest, mockResponse);
-            
-            expect(mockResponse.status).toHaveBeenCalledWith(200);
-            expect(mockResponse.json).toHaveBeenCalledWith({
-                message: expect.any(String),
-            });
-        });
+      expect(mockResponse.status).toHaveBeenCalledWith(400);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        error: expect.any(String),
+      });
     });
+  });
 
-    describe('#updateCategory() NotFound', () => {
-        beforeEach(async () => {
-            await Category.create({
-                nama: "Elektronik"
-            })
-        });
-        afterAll(async() => {
-            await Category.destroy({
-                where: {
-                    nama: "Elektronik",
-                }
-            })
-        });
-        it('Category not found 404 error', async () => {
-            const mockRequest = {
-                params: {
-                    id:2
-                }
-            };
-            const mockResponse = {
-                json: jest.fn().mockReturnThis(),
-                status: jest.fn().mockReturnThis(),
-            };
+  describe("#updateCategory() Success", () => {
+    it("should update the category success 200", async () => {
+      const mockRequest = {
+        params: {
+          id: 3,
+        },
+        body: {
+          nama: "Kendaraan",
+        },
+      };
+      const mockResponse = {
+        json: jest.fn().mockReturnThis(),
+        status: jest.fn().mockReturnThis(),
+      };
 
-            await categoryController.updateCategory(mockRequest, mockResponse);
-            
-            expect(mockResponse.status).toHaveBeenCalledWith(404);
-            expect(mockResponse.json).toHaveBeenCalledWith({
-                errors: [
-                    {
-                      code: expect.any(String),
-                      message: expect.any(String),
-                    },
-                  ],
-            });
-        });
+      await categoryController.updateCategory(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: expect.any(String),
+      });
     });
+    it("Category not found 404 error", async () => {
+      const mockRequest = {
+        params: {
+          id: 100,
+        },
+      };
+      const mockResponse = {
+        json: jest.fn().mockReturnThis(),
+        status: jest.fn().mockReturnThis(),
+      };
 
-    describe('#updateCategory() Exist', () => {
-        beforeEach(async () => {
-            await Category.create({
-                nama: "Elektronik"
-            })
-        });
-        afterAll(async() => {
-            await Category.destroy({
-                where: {
-                    nama: "Elektronik",
-                }
-            })
-        });
-        it('Category exist 400 error', async () => {
-            const mockRequest = {
-                params: {
-                    id:1
-                },
-                body: {
-                    nama: "Elektronik",
-                }
-            };
-            const mockResponse = {
-                json: jest.fn().mockReturnThis(),
-                status: jest.fn().mockReturnThis(),
-            };
+      await categoryController.updateCategory(mockRequest, mockResponse);
 
-            await categoryController.updateCategory(mockRequest, mockResponse);
-            
-            expect(mockResponse.status).toHaveBeenCalledWith(400);
-            expect(mockResponse.json).toHaveBeenCalledWith({
-                errors: [
-                    {
-                      code: expect.any(String),
-                      message: expect.any(String),
-                    },
-                  ],
-            });
-        });
+      expect(mockResponse.status).toHaveBeenCalledWith(404);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: expect.any(String),
+            message: expect.any(String),
+          },
+        ],
+      });
     });
+    it("Category exist 400 error", async () => {
+      const mockRequest = {
+        params: {
+          id: 3,
+        },
+        body: {
+          nama: "Elektronik",
+        },
+      };
+      const mockResponse = {
+        json: jest.fn().mockReturnThis(),
+        status: jest.fn().mockReturnThis(),
+      };
 
-    describe('#deleteCategory() Success', () => {
-        beforeEach(async () => {
-            await Category.create({
-                nama: "Elektronik"
-            })
-        });
-        afterAll(async() => {
-            await Category.destroy({
-                where: {
-                    nama: "Elektronik",
-                }
-            })
-        });
-        it('should delete a category success 200', async () => {
-            const mockRequest = {
-                params: {
-                    id: 1,
-                },
-            };
-            const mockResponse = {
-                json: jest.fn().mockReturnThis(),
-                status: jest.fn().mockReturnThis(),
-            };
+      await categoryController.updateCategory(mockRequest, mockResponse);
 
-            await categoryController.deleteCategory(mockRequest, mockResponse);
-
-            expect(mockResponse.status).toHaveBeenCalledWith(200);
-            expect(mockResponse.json).toHaveBeenCalledWith({
-                message: expect.any(String),
-            });
-        });
+      expect(mockResponse.status).toHaveBeenCalledWith(400);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: expect.any(String),
+            message: expect.any(String),
+          },
+        ],
+      });
     });
+    it("should return 400 error", async () => {
+      const mockRequest = {};
+      const mockResponse = {
+        json: jest.fn().mockReturnThis(),
+        status: jest.fn().mockReturnThis(),
+      };
 
-    describe('#deleteCategory() NotFound', () => {
-        it('Category is not found', async () => {
-            const mockRequest = {
-                params: {
-                    id: 12,
-                },
-            };
-            const mockResponse = {
-                json: jest.fn().mockReturnThis(),
-                status: jest.fn().mockReturnThis(),
-            };
+      await categoryController.updateCategory(mockRequest, mockResponse);
 
-            await categoryController.deleteCategory(mockRequest, mockResponse);
-
-            expect(mockResponse.status).toHaveBeenCalledWith(404);
-            expect(mockResponse.json).toHaveBeenCalledWith({
-                errors: [
-                    {
-                      code: expect.any(String),
-                      message: expect.any(String),
-                    },
-                  ],
-            });
-        });
+      expect(mockResponse.status).toHaveBeenCalledWith(400);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        error: expect.any(String),
+      });
     });
+  });
+  describe("#deleteCategory() Success", () => {
+    it("should delete a category success 200", async () => {
+      const mockRequest = {
+        params: {
+          id: 3,
+        },
+      };
+      const mockResponse = {
+        json: jest.fn().mockReturnThis(),
+        status: jest.fn().mockReturnThis(),
+      };
+
+      await categoryController.deleteCategory(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: expect.any(String),
+      });
+    });
+    it("Category is not found", async () => {
+      const mockRequest = {
+        params: {
+          id: 12,
+        },
+      };
+      const mockResponse = {
+        json: jest.fn().mockReturnThis(),
+        status: jest.fn().mockReturnThis(),
+      };
+
+      await categoryController.deleteCategory(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(404);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: expect.any(String),
+            message: expect.any(String),
+          },
+        ],
+      });
+    });
+    it("should return 400 if there's an error", async () => {
+      const mockRequest = {};
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      };
+
+      await categoryController.deleteCategory(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(400);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        error: expect.any(String),
+      });
+    });
+  });
 });
