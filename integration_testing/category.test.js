62b50d13be15ef18c78340a81f32a18ee9d14640
SatrioBayu <sbayu875@gmail.com>
2022-06-30 09:28:44 +0700

    feat: fix category integration testing

--- integration_testing/category.test.js
+++ integration_testing/category.test.js
@@ -4,27 +4,16 @@ const { Category } = require("../app/models");
 
 beforeAll(async () => {
   await Category.create({
-    id: 325,
-    nama: "Elektronik",
+    id: 3,
+    nama: "Peralatan Rumah",
   });
 });
 afterAll(async () => {
   await Category.destroy({
     where: {
-      id: 325,
-      nama: "Elektronik",
+      nama: "Otomotif",
     },
   });
-  await Category.update(
-    {
-      nama: "Elektronik",
-    },
-    {
-      where: {
-        id: 325,
-      },
-    }
-  );
 });
 
 //Category get
@@ -38,22 +27,13 @@ describe("/GET /categories", () => {
         expect(res.body.data).toBeDefined();
       });
   });
-  it("should return 204 not found", async () => {
-    return request(app)
-      .get("/categories")
-      .set("Content-type", "application/json")
-      .then((res) => {
-        expect(res.status).toBe(204);
-        expect(res.end).toBe();
-      });
-  });
 });
 
 //Category get by id
 describe("/GET /categories/:id", () => {
   it("should return a category 200 OK", async () => {
     return request(app)
-      .get("/categories/325")
+      .get("/categories/1")
       .set("Content-type", "application/json")
       .then((res) => {
         expect(res.status).toBe(200);
@@ -78,12 +58,9 @@ describe("/POST /categories", () => {
     return request(app)
       .post("/categories")
       .set("Content-type", "application/json")
-      .set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU"
-      )
+      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
       .send({
-        nama: "Elektronik",
+        nama: "Otomotif",
       })
       .then((res) => {
         expect(res.status).toBe(201);
@@ -95,12 +72,41 @@ describe("/POST /categories", () => {
     return request(app)
       .post("/categories")
       .set("Content-type", "application/json")
-      .set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU"
-      )
+      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
       .then((res) => {
         expect(res.status).toBe(400);
+        expect(res.body.errors).toBeDefined();
+      });
+  });
+  it("should return 400 if category already exist", async () => {
+    return request(app)
+      .post("/categories")
+      .set("Content-type", "application/json")
+      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
+      .send({
+        nama: "Elektronik",
+      })
+      .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.errors).toBeDefined();
+      });
+  });
+  it("should return 401 if there's no token", async () => {
+    return request(app)
+      .post("/categories")
+      .set("Content-type", "application/json")
+      .then((res) => {
+        expect(res.status).toBe(401);
+        expect(res.body.errors).toBeDefined();
+      });
+  });
+  it("should return 401 if token invalid", async () => {
+    return request(app)
+      .post("/categories")
+      .set("Content-type", "application/json")
+      .set("Authorization", "Bearer yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
+      .then((res) => {
+        expect(res.status).toBe(401);
         expect(res.body.errors).toBeDefined();
       });
   });
@@ -109,12 +115,12 @@ describe("/POST /categories", () => {
 describe("/PUT /categories/:id", () => {
   it("should return a category 200 OK", async () => {
     return request(app)
-      .put("/categories/325")
+      .put("/categories/3")
       .set("Content-type", "application/json")
-      .set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU"
-      )
+      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
+      .send({
+        nama: "Peralatan Rumah Tangga",
+      })
       .then((res) => {
         expect(res.status).toBe(200);
         expect(res.body.message).toBe("Category updated successfully");
@@ -124,10 +130,10 @@ describe("/PUT /categories/:id", () => {
     return request(app)
       .put("/categories/325")
       .set("Content-type", "application/json")
-      .set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU"
-      )
+      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
+      .send({
+        nama: "Peralatan Rumah Tangga",
+      })
       .then((res) => {
         expect(res.status).toBe(404);
         expect(res.body.errors).toBeDefined();
@@ -135,28 +141,73 @@ describe("/PUT /categories/:id", () => {
   });
   it("should return 400 bad request", async () => {
     return request(app)
-      .put("/categories/325")
+      .put("/categories/3")
       .set("Content-type", "application/json")
-      .set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU"
-      )
+      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
       .then((res) => {
         expect(res.status).toBe(400);
+        expect(res.body.errors).toBeDefined();
+      });
+  });
+  it("should return 400 bad request", async () => {
+    return request(app)
+      .put("/categories/3")
+      .set("Content-type", "application/json")
+      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
+      .send({
+        nama: "Elektronik",
+      })
+      .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.errors).toBeDefined();
+      });
+  });
+  it("should return 401 if there's no token", async () => {
+    return request(app)
+      .post("/categories")
+      .set("Content-type", "application/json")
+      .then((res) => {
+        expect(res.status).toBe(401);
+        expect(res.body.errors).toBeDefined();
+      });
+  });
+  it("should return 401 if token invalid", async () => {
+    return request(app)
+      .post("/categories")
+      .set("Content-type", "application/json")
+      .set("Authorization", "Bearer yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
+      .then((res) => {
+        expect(res.status).toBe(401);
         expect(res.body.errors).toBeDefined();
       });
   });
 });
 
 describe("/DELETE /categories/:id", () => {
+  it("should return 401 if there's no token", async () => {
+    return request(app)
+      .delete("/categories/3")
+      .set("Content-type", "application/json")
+      .then((res) => {
+        expect(res.status).toBe(401);
+        expect(res.body.errors).toBeDefined();
+      });
+  });
+  it("should return 401 if token invalid", async () => {
+    return request(app)
+      .delete("/categories/3")
+      .set("Content-type", "application/json")
+      .set("Authorization", "Bearer yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU2NDAwMTc2fQ.BrHyL3qdt1YmymSq5akBLc94CUaqRrqFe0ce4w3_ESA")
+      .then((res) => {
+        expect(res.status).toBe(401);
+        expect(res.body.errors).toBeDefined();
+      });
+  });
   it("should return a category 200 OK", async () => {
     return request(app)
-      .delete("/categories/325")
+      .delete("/categories/3")
       .set("Content-type", "application/json")
-      .set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU"
-      )
+      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU")
       .then((res) => {
         expect(res.status).toBe(200);
         expect(res.body.message).toBe("Category deleted successfully");
@@ -166,25 +217,9 @@ describe("/DELETE /categories/:id", () => {
     return request(app)
       .delete("/categories/325")
       .set("Content-type", "application/json")
-      .set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU"
-      )
+      .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU")
       .then((res) => {
         expect(res.status).toBe(404);
-        expect(res.body.errors).toBeDefined();
-      });
-  });
-  it("should return 400 bad request", async () => {
-    return request(app)
-      .delete("/categories/325")
-      .set("Content-type", "application/json")
-      .set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjU2NDY2NDM1fQ.0eP31AZz5HugRG5iPDxcnIJaK--J3AzhXX81axTl-QU"
-      )
-      .then((res) => {
-        expect(res.status).toBe(400);
         expect(res.body.errors).toBeDefined();
       });
   });
